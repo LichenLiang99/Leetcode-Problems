a627f431c62d77e0f321ca06629ea2a930b49dbf
Lichen Liang <90119777+LichenLiang99@users.noreply.github.com>
2021-09-14 04:28:50 -0400

    Time: 152 ms (88.89%), Space: 14.5 MB (89.52%) - LeetHub

---
+++ count-binary-substrings/count-binary-substrings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        pre, curr = 0, 1
+        count = 0
+        
+        #count the ones that belong to the same category 0 or 1 and store in current
+        #prev is the count of the previous set
+        for i in range(1, len(s)):
+            
+            if s[i] == s[i - 1]:
+                curr += 1
+            else:
+                pre = curr
+                curr = 1
+            
+            if pre >= curr:
+                count += 1
+        
+        return count
+    #time o(n) space o(1) concept string
