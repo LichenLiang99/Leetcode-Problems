bcf10a261fc8af9cb0fb789f987b70e3413119fe
Lichen Liang <90119777+LichenLiang99@users.noreply.github.com>
2021-09-14 04:16:59 -0400

    Time: 600 ms (86.41%), Space: 28 MB (93.30%) - LeetHub

---
+++ find-the-duplicate-number/find-the-duplicate-number.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        
+        #hashtable method
+        
+#         hashtable = defaultdict(int)
+        
+#         for i in nums:
+#             hashtable[i] += 1
+            
+#         for i in hashtable:
+#             if hashtable[i] != 1:
+#                 return i
+            
+        #Floyd's Cycle Detection
+        slow, fast = 0, 0
+        
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow2
