05bd2e1f9a1c99f858c01558884f15c1f871be9d
Lichen Liang <90119777+LichenLiang99@users.noreply.github.com>
2021-09-14 04:03:16 -0400

    Time: 70 ms (7.33%), Space: 15.2 MB (95.57%) - LeetHub

---
+++ path-sum-ii/path-sum-ii.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        stack = [(root, targetSum-root.val, [root.val])]
+        while stack:
+            curr, val, ls = stack.pop()
+            if not curr.left and not curr.right and val == 0:
+                res.append(ls)
+            if curr.right:
+                stack.append((curr.right, val-curr.right.val, ls+[curr.right.val]))
+            if curr.left:
+                stack.append((curr.left, val-curr.left.val, ls+[curr.left.val]))
+        return res 
+    
+    #time o(n) space o(n) concept tree, stack 
