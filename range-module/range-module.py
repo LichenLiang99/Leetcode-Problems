76c29e1d0455ad074ddbe39ceb83c6c570100b3b
Lichen Liang <90119777+LichenLiang99@users.noreply.github.com>
2021-12-11 15:43:59 -0500

    Time: 372 ms (82.97%), Space: 17.9 MB (96.51%) - LeetHub

---
+++ range-module/range-module.py
@@ -0,0 +1,38 @@
+class RangeModule:
+
+    def __init__(self):
+        self.X = []
+
+    def addRange(self, left: int, right: int) -> None:
+        start = bisect.bisect_left(self.X, left)
+        end = bisect.bisect_right(self.X, right)
+        subtrack = []
+        if start % 2 == 0:
+            subtrack.append(left)
+        if end % 2 == 0:
+            subtrack.append(right)
+        
+        self.X[start:end] = subtrack
+
+    def queryRange(self, left: int, right: int) -> bool:
+        start = bisect.bisect_right(self.X, left)
+        end = bisect.bisect_left(self.X, right)
+        return start == end and start % 2 == 1
+
+    def removeRange(self, left: int, right: int) -> None:
+        start = bisect.bisect_left(self.X, left)
+        end = bisect.bisect_right(self.X, right)
+        subtrack = []
+        if start % 2 == 1:
+            subtrack.append(left)
+        if end % 2 == 1:
+            subtrack.append(right)
+        
+        self.X[start:end] = subtrack
+
+
+# Your RangeModule object will be instantiated and called as such:
+# obj = RangeModule()
+# obj.addRange(left,right)
+# param_2 = obj.queryRange(left,right)
+# obj.removeRange(left,right)
