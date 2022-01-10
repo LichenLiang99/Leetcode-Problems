d10a83a30cc0d5ba39fa1da69300b23c556c38ce
Lichen Liang <90119777+LichenLiang99@users.noreply.github.com>
2022-01-10 17:41:43 -0500

    Time: 270 ms (17.04%), Space: 16.6 MB (86.19%) - LeetHub

---
+++ 136-single-number/136-single-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        hashtable = defaultdict(int)
+#         for i in nums:
+#             hashtable[i] += 1
+        
+#         for i in hashtable:
+#             if hashtable[i] == 1:
+#                 return i
+        
+        for i in nums:
+            hashtable[i] += 1
+            if hashtable[i] == 2:
+                del hashtable[i]
+                
+        key = list(hashtable.keys())[0]
+        return key
+        #xor
+        # res = 0
+        # for n in nums:
+        #     res ^= n
+        # return res
