2879034526043df3c5be8402f470c469b1242184
Lichen Liang <90119777+LichenLiang99@users.noreply.github.com>
2022-01-15 16:20:19 -0500

    Time: 57 ms (17.67%), Space: 14.3 MB (32.88%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.py
@@ -0,0 +1,27 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count = collections.Counter(s)
+        maxHeap = [[-cnt, c] for c, cnt in count.items()]
+        
+        heapq.heapify(maxHeap)
+        prev = None
+        res = ""
+        
+        while maxHeap or prev:
+            #most freq except prev
+            
+            if prev and not maxHeap:
+                return ""
+            
+            cnt, c = heapq.heappop(maxHeap)
+            res += c
+            cnt += 1
+            
+            if prev:
+                heapq.heappush(maxHeap, prev)
+                prev = None
+            
+            if cnt != 0:
+                prev = [cnt, c]
+            
+        return res
