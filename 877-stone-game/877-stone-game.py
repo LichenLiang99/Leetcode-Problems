f6528f0124929509dd4d1c17c56ecda04f95037d
Lichen Liang <90119777+LichenLiang99@users.noreply.github.com>
2022-01-21 09:22:19 -0500

    Time: 1667 ms (5.03%), Space: 127 MB (5.29%) - LeetHub

--- 877-stone-game/877-stone-game.py
+++ 877-stone-game/877-stone-game.py
@@ -1,3 +1,23 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        #return True
+        
+        dp = {} #subarr (l,r) : max Alice
+        
+        #return max alice total
+        def dfs(l, r):
+            if l > r:
+                return 0
+            
+            if (l,r) in dp:
+                return dp[(l,r)]
+            
+            even = True if r-l % 2 else False
+            left = piles[l] if even else 0
+            right = piles[r] if even else 0
+            
+            dp[(l,r)] = max((dfs(l+1, r) + left), dfs(l, r-1) + right)
+            
+            return dp[(l,r)]
+        
+        return dfs(0, len(piles) - 1)
