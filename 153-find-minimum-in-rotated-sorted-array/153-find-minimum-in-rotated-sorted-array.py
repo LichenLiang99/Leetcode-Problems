252fec203043917b1b848df3bfd6621237edd3b3
Lichen Liang <90119777+LichenLiang99@users.noreply.github.com>
2022-01-29 23:39:50 -0500

    Time: 71 ms (17.70%), Space: 14.1 MB (99.20%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -1,26 +1,26 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-#         res = nums[0]
-#         l = 0
-#         r = len(nums) - 1
+        res = nums[0]
+        l = 0
+        r = len(nums) - 1
         
-#         while l <= r:
-#             #if current array is sorted, just return leftmost
-#             if nums[l] < nums[r]:
-#                 res = min(res, nums[l])
-#                 break
+        while l <= r:
+            #if current array is sorted, just return leftmost
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
                 
-#             #if current array is not sorted
-#             mid = l + (r - l) // 2
-#             res = min(res, nums[mid])
+            #if current array is not sorted
+            mid = l + (r - l) // 2
+            res = min(res, nums[mid])
             
-#             #if mid belongs to left portion, search the right
-#             if nums[mid] >= nums[l]:
-#                 l = mid + 1
-#             else:
-#                 r = mid - 1
+            #if mid belongs to left portion, search the right
+            if nums[mid] >= nums[l]:
+                l = mid + 1
+            else:
+                r = mid - 1
             
-#         return res
-        return min(nums)
+        return res
+
     
     #time o(logn) space o(1) concept binary search
