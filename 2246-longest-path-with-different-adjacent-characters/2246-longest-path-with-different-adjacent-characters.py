44d75b4e408893b421dd89b49c4c2a86727f9097
Lichen Liang <liang4400@gmail.com>
2022-05-18 19:58:14 -0400

    Time: 2427 ms (52.15%), Space: 259.3 MB (5.02%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -1,26 +1,26 @@
 class Solution:
     def longestPath(self, parent: List[int], s: str) -> int:
-        ans = 0
-        children = {i:[] for i in range(len(parent))}
+        result = 0
+        children = defaultdict(list)
         for i in range(1,len(parent)):
             children[parent[i]].append(i)
 
-        def trav(v, prev_c):
-            nonlocal ans, children
-            if prev_c == s[v]:
-                trav(v,"")
+        def dfs(v, prev_child):
+            nonlocal result, children
+            if prev_child == s[v]:
+                dfs(v,"")
                 return 0
             else:
                 maxl, smaxl = 0,0
                 for i in children[v]:
-                    x = trav(i,s[v])
+                    x = dfs(i,s[v])
                     if x >= maxl:
                         smaxl = maxl
                         maxl = x
                     elif x > smaxl:
                         smaxl = x
-                ans = max(ans,1+maxl+smaxl)
+                result = max(result,1+maxl+smaxl)
                 return 1+maxl
         
-        trav(0,"")
-        return ans
+        dfs(0,"")
+        return result
