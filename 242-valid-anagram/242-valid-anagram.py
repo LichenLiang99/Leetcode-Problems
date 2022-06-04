02242691e6f44d33a93835c3668ded7bb6e15fa8
Lichen Liang <liang4400@gmail.com>
2022-06-04 17:47:16 -0400

    Time: 52 ms (83.82%), Space: 14.5 MB (34.62%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+#         ht = defaultdict(int)
+        
+#         #increase counter
+#         for i in s:
+#             ht[i] += 1
+        
+#         #if not in hashtable, false, else decrese its counter
+#         for i in t:
+#             if i not in ht:
+#                 return False
+#             else:
+#                 ht[i] -= 1
+              
+#         #make sure all entries are 0
+#         for i in ht:
+#             if ht[i] != 0:
+#                 return False
+            
+#         return True
+        return Counter(s) == Counter(t)
+    
+        #time o(n) space o(n) concept string, hash table
